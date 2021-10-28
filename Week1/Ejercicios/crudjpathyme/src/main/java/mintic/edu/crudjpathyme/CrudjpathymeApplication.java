25fd5fc6fc816f7ec5fee9c80e108665f8030b96
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-10-28 15:56:58 -0500

    Spring boot with Maven project created

---
+++ Week1/Ejercicios/crudjpathyme/src/main/java/mintic/edu/crudjpathyme/CrudjpathymeApplication.java
@@ -0,0 +1,13 @@
+package mintic.edu.crudjpathyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudjpathymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudjpathymeApplication.class, args);
+	}
+
+}
---
+++ Week1/Ejercicios/crudjpathyme/src/test/java/mintic/edu/crudjpathyme/CrudjpathymeApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.edu.crudjpathyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudjpathymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
