24860760a32230463bce3064749d41b87a384496
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-11-02 16:26:45 -0500

    Usuario creado

--- Proyecto/proyectobase/src/main/java/mintic/edu/proyectobase/modelo/Usuario.java
+++ Proyecto/proyectobase/src/main/java/mintic/edu/proyectobase/modelo/Usuario.java
@@ -1,5 +1,31 @@
 package mintic.edu.proyectobase.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+    
+    // Atributos
+    @Id
+    private int idUsuario;
+    private String nombreUsuario;
+    private String clave;
+    private String correo;
+    private String tipoUsuario;
+    
+    
+    public Usuario() {
+    }
+
+
+    public Usuario(int idUsuario, String nombreUsuario, String clave, String correo, String tipoUsuario) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.clave = clave;
+        this.correo = correo;
+        this.tipoUsuario = tipoUsuario;
+    }
+
     
 }
