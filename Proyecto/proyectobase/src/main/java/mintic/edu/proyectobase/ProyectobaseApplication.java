e87c4aec6d6bf19e42d463972e5b7aa4ca3034ed
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-11-02 15:52:58 -0500

    Proyecto ciclo 4 creado

---
+++ Proyecto/proyectobase/src/main/java/mintic/edu/proyectobase/ProyectobaseApplication.java
@@ -0,0 +1,13 @@
+package mintic.edu.proyectobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectobaseApplication.class, args);
+	}
+
+}
