faa25f8ae83f63aa831de8d52d97d86286d5d8f8
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-11-04 15:30:29 -0500

    proyectoFront

---
+++ Week2/Clases/proyectofront/src/main/java/com/front/proyectofront/ProyectofrontApplication.java
@@ -0,0 +1,13 @@
+package com.front.proyectofront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectofrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectofrontApplication.class, args);
+	}
+
+}
---
+++ Week2/Clases/proyectofront/src/test/java/com/front/proyectofront/ProyectofrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.front.proyectofront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectofrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
